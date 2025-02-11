f295a91810cc1f58c6b1e2f0da997d6b4dbbcccf
JeremyPanaiva <jeremy.panaiva@etu.univ-amu.fr>
2025-02-11 17:22:08 +0100

    modif du 5

--- src/yaip5/Circle.java
+++ src/yaip5/Circle.java
@@ -4,5 +4,20 @@ public class Circle {
 	double radius ;
 	String color  ;
 	
+	public Circle(double radius, String color) {
+		this.radius = radius;
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + ", color=" + color + "]";
+	}
+	
+	
+	public static void main (String[] args) {
+		Circle c1 = new Circle(2,"rouge") ;
+		System.out.println(c1);
+	}
 	
 }
--- src/yaip5/Cylinder.java
+++ src/yaip5/Cylinder.java
@@ -5,4 +5,28 @@ public class Cylinder {
 		   private Circle base;   // Base circle, an instance of Circle class
 		   private double height;
 		   
+		   
+		public Cylinder(Circle base, double height) {
+			this.base = base;
+			this.height = height;
+		}
+		
+		public Cylinder(double radius,String color,double height) {
+			this.base=new Circle(radius,color);
+			this.height=height ;
+		}
+
+
+		@Override
+		public String toString() {
+			return "Cylinder [base=" + base + ", height=" + height + "]";
+		}
+		   
+		public static void main (String[] args) {
+			Cylinder cy3= new Cylinder(20,"bleu",1);
+			Circle c1 = new Circle(2,"rouge");
+			Cylinder cy2 = new Cylinder (c1,10) ;
+			System.out.println(c1);
+			System.out.println(cy3);
+		}
 }
