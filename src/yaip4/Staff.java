8ab82f3463b8b112d42f3c508eac79a19778af8c
JeremyPanaiva <jeremy.panaiva@etu.univ-amu.fr>
2025-02-11 15:48:31 +0100

    ajout 4.2

---
+++ src/yaip4/Staff.java
@@ -0,0 +1,39 @@
+package yaip4;
+
+public class Staff extends Person {
+	String school ;
+	double pay ;
+	
+	
+	public Staff(String name, String address, String school, double pay) {
+		super(name, address);
+		this.school = school;
+		this.pay = pay;
+	}
+	
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public double getPay() {
+		return pay;
+	}
+	public void setPay(double pay) {
+		this.pay = pay;
+	}
+
+	@Override
+	public String toString() {
+		return "Staff "+ super.toString() +"[school=" + school + ", pay=" + pay + "]";
+	} 
+	
+	
+	public static void main (String[] args) {
+		Staff monsta = new Staff ("lili","lolo","havard",9.0);
+		System.out.println(monsta);
+		
+	}
+	
+}
