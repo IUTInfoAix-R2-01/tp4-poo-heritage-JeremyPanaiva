55f086c4fc13bebb9ccbc63c10e76314ea8fd884
JeremyPanaiva <jeremy.panaiva@etu.univ-amu.fr>
2025-02-12 10:52:25 +0100

    fin du yaip 6

---
+++ src/yaip6/BigDog.java
@@ -0,0 +1,21 @@
+package yaip6;
+
+public class BigDog extends Dog{
+	
+	public BigDog() {
+		super(name);
+	}
+	
+	public void greets() {
+		System.out.println("Wooow");
+	}
+	
+	public void greets(Dog another) {
+		System.out.println("Woooooow");
+	}
+	
+	public void greets(BigDog another) {
+		System.out.println("Woooooooooooooooooow");
+	}
+}
+
---
+++ src/yaip6/Movable.java
@@ -0,0 +1,8 @@
+package yaip6;
+
+public interface Movable {
+	public void moveUp();
+	public void moveLeft();
+	public void moveRight();
+	public void moveDown();
+}
